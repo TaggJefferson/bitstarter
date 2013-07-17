4c52ac88e120f6f39d34814fa422f1ac7ffc9848
taggjefferson@gmail.com <ubuntu@ip-172-31-18-142.us-west-2.compute.internal>
2013-07-17 19:42:33 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -2,10 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync(index.html)
-
 app.get('/', function(request, response) {
-  response.send(but.toString('utf8', 0, text));
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
